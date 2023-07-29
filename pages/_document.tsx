53a5f88d199d32d8400b73da2ec4fed28f687b08
xiaoke <xiaoke@micous.com>
2023-07-29 13:01:50 +0800

    fix: prettier style causes build errors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,18 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/favicon-76x76.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/jpg" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/jpg"
+            sizes="16x16"
+            href="/static/favicons/favicon-16x16.png"
+          />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/favicon-32x32.png" color="#fff" />
           <meta name="msapplication-TileColor" content="#000000" />
@@ -15,8 +25,14 @@ class MyDocument extends Document {
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body className="antialiased text-black bg-white dark:bg-dark dark:text-white">
           <Main />
