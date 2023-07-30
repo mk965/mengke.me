1554f47f6f5738be074fb777cfda3df928d2a479
xiaoke <xiaoke@micous.com>
2023-07-30 12:12:15 +0800

    fix: Umami Analytics script url

--- components/analytics/SimpleAnalytics.tsx
+++ components/analytics/SimpleAnalytics.tsx
@@ -7,12 +7,7 @@ export function SimpleAnalyticsScript() {
       <Script strategy="lazyOnload" id="sa-script">
         {`window.sa_event=window.sa_event||function(){var a=[].slice.call(arguments);window.sa_event.q?window.sa_event.q.push(a):window.sa_event.q=[a]};`}
       </Script>
-      {/* <Script strategy="lazyOnload" src="https://scripts.simpleanalyticscdn.com/latest.js" /> */}
-      <Script
-        async
-        src="https://analytics.umami.is/script.js"
-        data-website-id={siteMetadata.analytics.umamiWebsiteId}
-      ></Script>
+      <Script strategy="lazyOnload" src="https://scripts.simpleanalyticscdn.com/latest.js" />
     </>
   )
 }
