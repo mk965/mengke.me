cef2c645ea9c550347cadb15769c8339fce5dd0b
mk965 <mencre@163.com>
2023-08-17 10:08:51 +0800

    update(friends): Add some friends link

--- data/friendsData.ts
+++ data/friendsData.ts
@@ -29,4 +29,18 @@ export let friendsData: Friend[] = [
     imgSrc: '/static/images/friends/kkjz.jpg',
     url: 'https://kkjz.xyz/?ref=mengke.me',
   },
+  {
+    type: 'friend',
+    name: '什么值得读',
+    slogan: '互联网人的书单库！',
+    imgSrc: '/static/images/friends/shenmezhidedu.png',
+    url: 'https://www.shenmezhidedu.com/?ref=mengke.me',
+  },
+  {
+    type: 'friend',
+    name: 'BestXTools',
+    slogan: '发现并分享有趣，有创意，免费、好用的工具的非商业性质的，独立的，开放的社区。',
+    imgSrc: '/static/images/friends/bestxtools.png',
+    url: 'https://discuss-cn.bestxtools.com/?ref=mengke.me',
+  },
 ]
