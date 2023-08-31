4f5c4800964e8a5589990152e5fb7ebd8f951a74
mk965 <mencre@163.com>
2023-08-31 11:29:06 +0800

    fix: Friends Page Title

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -4,13 +4,13 @@ import { friendsData } from '~/data/friendsData'
 import { siteMetadata } from '~/data/siteMetadata'
 
 export default function Friends() {
-  let friends = friendsData.filter(({ type }) => type === 'friend')
-  let techStars = friendsData.filter(({ type }) => type === 'techStar')
-  let description = 'My friends and the tech bloggers I recommend'
+  const friends = friendsData.filter(({ type }) => type === 'friend')
+  const techStars = friendsData.filter(({ type }) => type === 'techStar')
+  const description = 'My friends and the tech bloggers I recommend'
 
   return (
     <>
-      <PageSeo title={`Projects - ${siteMetadata.author}`} description={description} />
+      <PageSeo title={`Friends - ${siteMetadata.author}`} description={description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
