2da96849d73dd132a3615050598d9d28e0c6391f
mk965 <mencre@163.com>
2023-09-29 22:26:08 +0800

    docs(friends): Add friend

--- data/friendsData.ts
+++ data/friendsData.ts
@@ -3,6 +3,13 @@ import type { Friend } from '~/types'
 export let friendsData: Friend[] = [
   {
     type: 'friend',
+    name: '年年无忧·岁岁平安',
+    slogan: '年年无忧·岁岁平安',
+    imgSrc: '/static/images/friends/suisuipingan.jpg',
+    url: 'http://suisuipingan.love/home/?ref=mengke.me',
+  },
+  {
+    type: 'friend',
     name: 'Kong',
     slogan: '当机立断，非黑即白，不要后悔',
     imgSrc: '/static/images/friends/kong.jpg',
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,15 +4,16 @@ import { projectsData } from '~/data/projectsData'
 import { siteMetadata } from '~/data/siteMetadata'
 
 export default function Projects() {
-  let workProjects = projectsData.filter(({ type }) => type === 'work')
-  let sideProjects = projectsData.filter(({ type }) => type === 'self')
-  let description = 'My open-source side projects and stuff that I built with my colleagues at work'
+  const workProjects = projectsData.filter(({ type }) => type === 'work')
+  const sideProjects = projectsData.filter(({ type }) => type === 'self')
+  const description =
+    'My open-source side projects and stuff that I built with my colleagues at work'
 
   return (
     <>
       <PageSeo title={`Projects - ${siteMetadata.author}`} description={description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Projects
           </h1>
@@ -22,7 +23,7 @@ export default function Projects() {
           <h3 className="mb-4 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100">
             Work
           </h3>
-          <div className="-m-4 flex flex-wrap">
+          <div className="flex flex-wrap -m-4">
             {workProjects.map((project) => (
               <ProjectCard key={project.title} project={project} />
             ))}
@@ -32,7 +33,7 @@ export default function Projects() {
           <h3 className="mb-4 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100">
             Side projects
           </h3>
-          <div className="-m-4 flex flex-wrap">
+          <div className="flex flex-wrap -m-4">
             {sideProjects.map((project) => (
               <ProjectCard key={project.title} project={project} />
             ))}
