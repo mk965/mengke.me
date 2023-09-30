64068320ae242e6b8b9762315fa1dfe1a7c319d7
mk965 <mencre@163.com>
2023-09-30 21:28:14 +0800

    feat(snippets): Getting Spotify token

---
+++ pages/snippets/[...slug].tsx
@@ -0,0 +1,56 @@
+import { MDXLayoutRenderer } from 'components/MDXComponents'
+import { PageTitle } from '~/components/PageTitle'
+import { getCommentConfigs } from '~/libs/comment'
+import { formatSlug, getFiles } from '~/libs/files'
+import { getFileBySlug } from '~/libs/mdx'
+import type { MdxPageLayout } from '~/types/mdx'
+import type { SnippetProps } from '~/types/page'
+
+let DEFAULT_LAYOUT: MdxPageLayout = 'PostSimple'
+
+export async function getStaticPaths() {
+  let snippets = getFiles('snippets')
+  return {
+    paths: snippets.map((p: string) => ({
+      params: {
+        slug: formatSlug(p).split('/'),
+      },
+    })),
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  let snippet = await getFileBySlug('snippets', params.slug.join('/'))
+  let commentConfig = getCommentConfigs()
+  return {
+    props: { snippet, commentConfig },
+  }
+}
+
+export default function Snippet({ snippet, commentConfig }: SnippetProps) {
+  let { mdxSource, frontMatter } = snippet
+
+  return (
+    <>
+      {frontMatter.draft !== true ? (
+        <MDXLayoutRenderer
+          layout={frontMatter.layout || DEFAULT_LAYOUT}
+          mdxSource={mdxSource}
+          type="snippets"
+          frontMatter={frontMatter}
+          commentConfig={commentConfig}
+        />
+      ) : (
+        <div className="mt-24 text-center">
+          <PageTitle>
+            Under construction
+            <span role="img" aria-label="roadwork sign">
+              🚧
+            </span>
+          </PageTitle>
+        </div>
+      )}
+    </>
+  )
+}
--- pages/snippets/index.tsx
+++ pages/snippets/index.tsx
@@ -1,26 +1,22 @@
-import Image from 'next/image'
-import { Link } from '~/components/Link'
-import { Twemoji } from '~/components/Twemoji'
+import { PageSeo } from 'components/SEO'
+import { SnippetLayout } from '~/layouts/SnippetLayout'
+import { getAllFilesFrontMatter } from '~/libs/mdx'
+import type { SnippetFrontMatter } from '~/types/mdx'
+import { siteMetadata } from '~/data/siteMetadata'
 
-export default function FourZeroFour() {
+export async function getStaticProps() {
+  let snippets = getAllFilesFrontMatter(`/snippets`)
+  return {
+    props: { snippets },
+  }
+}
+
+export default function Snippet({ snippets }: { snippets: SnippetFrontMatter[] }) {
+  let description = 'My snippets collection'
   return (
-    <div className="flex flex-col items-center justify-center pt-4 md:pt-10 xl:pt-20">
-      <div>
-        <Image src={'/static/images/coding.png'} alt="coding" width={500} height={500} />
-      </div>
-      <div className="pt-8 space-x-2 md:space-y-5 md:pt-12 xl:pt-16">
-        <div className="max-w-md text-center">
-          <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-            This page is under development... <Twemoji emoji={'face-with-monocle'} />
-          </p>
-          <p className="mb-8">I'll put some interesting/practical code snippets here.</p>
-          <Link href="/">
-            <button className="inline px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg shadow focus:shadow-outline-blue hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-              Back to homepage
-            </button>
-          </Link>
-        </div>
-      </div>
-    </div>
+    <>
+      <PageSeo title={`Snippets - ${siteMetadata.author} - Snippets`} description={description} />
+      <SnippetLayout snippets={snippets} description={description} />
+    </>
   )
 }
