70d0fc0e95464f06856647abeba83be96f8e0343
mk965 <mencre@163.com>
2023-12-21 17:33:17 +0800

    feat: add BuiltWith

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,10 @@ import { BuiltWith } from './BuiltWith'
 export function Footer() {
   return (
     <footer>
-      <div className="mb-8 mt-16 items-center justify-between space-y-4 md:mb-10 md:flex md:space-y-0">
-        {/* <BuiltWith /> */}
+      <div className="items-center justify-between mt-16 mb-8 space-y-4 md:mb-10 md:flex md:space-y-0">
+        <BuiltWith />
         <div></div>
-        <div className="my-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="flex my-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{`Copyright © ${new Date().getFullYear()}`}</div>
           <span>{` • `}</span>
           <span>{siteMetadata.footerTitle}</span>
