267c83429939b06e75e9e3849fe91ccda21514dd
mk965 <mencre@163.com>
2023-12-21 17:04:49 +0800

    docs(post): add one post

---
+++ components/CodePenSnippet.tsx
@@ -0,0 +1,21 @@
+export default function CodePenSnippet(props: { snippetId: string }) {
+  const snippetId = props.snippetId
+  return (
+    snippetId && (
+      <div className="w-full overflow-hidden rounded-lg shadow-nextjs dark:shadow-nextjs-dark">
+        <iframe
+          title="Codepen"
+          src={`https://codepen.io/mk965/full/${snippetId}`}
+          style={{
+            width: '100%',
+            height: '500px',
+            border: '0',
+            overflow: 'hidden',
+          }}
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        />
+      </div>
+    )
+  )
+}
