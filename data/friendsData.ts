6a95b45f05cf97344c50c89efd4acf2b0ea9efc8
mk965 <mencre@163.com>
2024-04-07 17:14:56 +0800

    docs(friends): delete some friends

--- data/friendsData.ts
+++ data/friendsData.ts
@@ -8,13 +8,13 @@ export let friendsData: Friend[] = [
     imgSrc: '/static/images/friends/suisuipingan.jpg',
     url: 'http://suisuipingan.love/home/?ref=mengke.me',
   },
-  {
-    type: 'friend',
-    name: 'Kong',
-    slogan: '当机立断，非黑即白，不要后悔',
-    imgSrc: '/static/images/friends/kong.jpg',
-    url: 'https://kongf.github.io/?ref=mengke.me',
-  },
+  // {
+  //   type: 'friend',
+  //   name: 'Kong',
+  //   slogan: '当机立断，非黑即白，不要后悔',
+  //   imgSrc: '/static/images/friends/kong.jpg',
+  //   url: 'https://kongf.github.io/?ref=mengke.me',
+  // },
   {
     type: 'friend',
     name: 'UoCat',
@@ -43,28 +43,6 @@ export let friendsData: Friend[] = [
     imgSrc: '/static/images/friends/shenmezhidedu.png',
     url: 'https://www.shenmezhidedu.com/?ref=mengke.me',
   },
-  {
-    type: 'friend',
-    name: 'BestXTools',
-    slogan: '发现并分享有趣，有创意，免费、好用的工具的非商业性质的，独立的，开放的社区。',
-    imgSrc: '/static/images/friends/bestxtools.png',
-    url: 'https://discuss-cn.bestxtools.com/?ref=mengke.me',
-  },
-  {
-    type: 'friend',
-    name: "RE1IFE's Blog",
-    slogan: '一个淹死在时代浪潮里的 coder',
-    imgSrc: '/static/images/friends/re1ife.jpg',
-    url: 'http://www.re1ife.top/?ref=mengke.me',
-  },
-  {
-    type: 'friend',
-    name: '鄢云峰的个人网站',
-    slogan: 'Enjoy Life, Enjoy Coding',
-    imgSrc: '/static/images/friends/yanyunfeng.jpeg',
-    url: 'https://yanyunfeng.com/?ref=mengke.me',
-  },
-
   {
     type: 'techStar',
     name: 'Anthony Fu',
