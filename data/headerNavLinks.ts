9b673c14e80d245e3b8436d9fe8c39a32fc06e4a
mk965 <mencre@163.com>
2024-05-03 21:23:16 +0800

    feat(page): added Health Status page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,8 @@
 export let headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/gallery', title: 'Gallery' },
-  { href: '/snippets', title: 'Snippets' },
+  { href: '/status/health', title: 'Status' },
+  // { href: '/gallery', title: 'Gallery' },
+  // { href: '/snippets', title: 'Snippets' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
   { href: '/friends', title: 'Friends' },
