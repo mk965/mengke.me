6fef6274c8be6f1fd689b1932d33fb2d6c89c5e3
mk965 <mencre@163.com>
2024-10-23 14:32:39 +0800

    style: adjust grid spacing and adjust padding for DiscussAndEdit component in PostLayout

--- layouts/post-layout.tsx
+++ layouts/post-layout.tsx
@@ -1,9 +1,11 @@
 import type { Author, Blog } from 'contentlayer/generated'
 import type { CoreContent } from 'pliny/utils/contentlayer'
 import type { ReactNode } from 'react'
+import { BackToPosts } from '~/components/blog/back-to-posts'
 import { Banner } from '~/components/blog/banner'
 import { BlogMeta } from '~/components/blog/blog-meta'
 import { Comments } from '~/components/blog/comments'
+import { DiscussAndEdit } from '~/components/blog/discuss-and-edit'
 import { PostNav } from '~/components/blog/post-nav'
 import { PostTitle } from '~/components/blog/post-title'
 import { ScrollButtons } from '~/components/blog/scroll-buttons'
@@ -22,8 +24,8 @@ interface LayoutProps {
 }
 
 export function PostLayout({ content, next, prev, children }: LayoutProps) {
-  const { slug, images, lastmod, readingTime, date, title, tags, toc, type } = content
-  const postUrl = `${SITE_METADATA.siteUrl}/${type.toLowerCase()}/${slug}`
+  let { slug, images, lastmod, readingTime, date, title, tags, filePath, toc, type } = content
+  let postUrl = `${SITE_METADATA.siteUrl}/${type.toLowerCase()}/${slug}`
 
   return (
     <Container className="pt-4 lg:pt-12">
@@ -41,15 +43,17 @@ export function PostLayout({ content, next, prev, children }: LayoutProps) {
               <SocialShare postUrl={postUrl} title={title} />
             </div>
           </div>
-          <div className="grid grid-cols-1 gap-8 pb-10 pt-8 lg:grid-cols-12 lg:pt-10">
+          <div className="grid grid-cols-1 gap-12 pb-10 pt-8 lg:grid-cols-12 lg:pt-10">
             <div className="divide-y divide-gray-200 dark:divide-gray-700 lg:col-span-8 xl:col-span-9">
               <div className="prose max-w-none dark:prose-invert lg:prose-lg lg:pb-8">
                 {children}
               </div>
             </div>
             <div className="hidden lg:col-span-4 lg:block xl:col-span-3">
-              <div className="max-h-[88vh] overflow-y-auto lg:sticky lg:top-24">
-                <TableOfContents toc={toc} />
+              <div className="space-y-4 divide-y divide-gray-200 dark:divide-gray-700 lg:sticky lg:top-24">
+                <BackToPosts label="Back to posts" />
+                <TableOfContents toc={toc} className="pt-4" />
+                <DiscussAndEdit postUrl={postUrl} filePath={filePath} className="pt-4" />
               </div>
             </div>
           </div>
