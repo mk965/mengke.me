801c132b82a13f7d81a5dd35f4c0931cf547eafa
mk965 <mencre@163.com>
2025-08-02 23:51:37 +0800

    feat(project-card): optimized project card display logic

--- components/cards/project/index.tsx
+++ components/cards/project/index.tsx
@@ -2,7 +2,6 @@
 
 import clsx from 'clsx'
 import { Github, Download } from 'lucide-react'
-import { Fragment } from 'react'
 import useSWR from 'swr'
 import type { BrandsMap } from '~/components/ui/brand'
 import { Brand } from '~/components/ui/brand'
@@ -15,21 +14,85 @@ import type { PROJECTS } from '~/data/projects'
 import type { GithubRepository, NpmPackage } from '~/types/data'
 import { fetcher } from '~/utils/misc'
 
+function NpmStats({ npmPackageName, downloads }: { npmPackageName: string; downloads: number }) {
+  return (
+    <div className="space-y-1.5">
+      <div className="text-xs text-gray-600 dark:text-gray-400">
+        <span className="hidden sm:inline">Monthly downloads</span>
+        <span className="sm:hidden">Downloads</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <Link
+          href={`https://www.npmjs.com/package/${npmPackageName}`}
+          className="flex items-center gap-1.5"
+        >
+          <GrowingUnderline data-umami-event="project-npm-link">
+            <div className="flex items-center space-x-1.5">
+              <Download size={16} strokeWidth={1.5} />
+              <span className="font-medium">{downloads}</span>
+            </div>
+          </GrowingUnderline>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+function GithubStats({ repository }: { repository: GithubRepository }) {
+  return (
+    <div className="space-y-1.5">
+      <div className="text-xs text-gray-600 dark:text-gray-400">
+        <span className="hidden sm:inline">Github stars</span>
+        <span className="sm:hidden">Stars</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <Link href={repository.url} className="flex items-center gap-1.5">
+          <GrowingUnderline data-umami-event="project-github-link">
+            <div className="flex items-center space-x-1.5">
+              <Github size={16} strokeWidth={1.5} />
+              <span className="font-medium">{repository.stargazerCount}</span>
+            </div>
+          </GrowingUnderline>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+function Stack({ builtWith }: { builtWith: string[] }) {
+  return (
+    <div className="space-y-1.5">
+      <div className="text-xs text-gray-600 dark:text-gray-400">Stack</div>
+      <div className="flex h-6 flex-wrap items-center gap-1.5">
+        {builtWith?.map((tool) => {
+          return (
+            <Brand
+              key={tool}
+              name={tool as keyof typeof BrandsMap}
+              iconClassName={clsx(tool === 'Pygame' ? 'h-4' : 'h-4 w-4')}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
 export function ProjectCard({ project }: { project: (typeof PROJECTS)[0] }) {
-  const { title, description, imgSrc, url, repo, npm, builtWith, links } = project
+  const { title, description, imgSrc, url, repo, npmPackageName, builtWith, links } = project
 
   const { data: repository } = useSWR<GithubRepository>(
-    repo && typeof repo === 'string' ? `/api/github?repo=${repo}` : null,
+    repo ? `/api/github?repo=${repo}` : null,
     fetcher
   )
 
   const { data: npmPackage } = useSWR<NpmPackage>(
-    npm && typeof npm === 'string' ? `/api/npm?package=${npm}` : null,
+    npmPackageName ? `/api/npm?package=${npmPackageName}` : null,
     fetcher
   )
-
   const href = url || repository?.url
-  const lang = repository?.languages?.[0]
+
+  const propertyCount = (npmPackage ? 1 : 0) + (repository ? 1 : 0) + (builtWith.length > 0 ? 1 : 0)
 
   return (
     <GradientBorder
@@ -51,101 +114,21 @@ export function ProjectCard({ project }: { project: (typeof PROJECTS)[0] }) {
           </h2>
         </div>
       </div>
-      <p className="mb-16 line-clamp-3 grow text-lg">{repository?.description || description}</p>
-      <div className={clsx('mt-auto flex gap-6 sm:gap-9 md:grid md:gap-0', `grid-cols-3`)}>
-        {npmPackage
-          ? // NPM Downloads
-            (npmPackage || (npm && typeof npm === 'object')) && (
-              <div className="space-y-1.5">
-                <div className="text-xs text-gray-600 dark:text-gray-400">
-                  <span className="hidden sm:inline">Monthly downloads</span>
-                  <span className="sm:hidden">Downloads</span>
-                </div>
-                <div className="flex items-center gap-2">
-                  <div className="flex items-center space-x-1.5">
-                    <Download size={16} strokeWidth={1.5} />
-                    <span className="font-medium">
-                      {npmPackage?.downloads ||
-                        (npm && typeof npm === 'object' ? npm.downloads : 0)}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            )
-          : repository
-            ? // GitHub Stars
-              (repository || (repo && typeof repo === 'object')) && (
-                <div className="space-y-1.5">
-                  <div className="text-xs text-gray-600 dark:text-gray-400">
-                    <span className="hidden sm:inline">Github stars</span>
-                    <span className="sm:hidden">Stars</span>
-                  </div>
-                  <div className="flex items-center gap-2">
-                    <div className="flex items-center space-x-1.5">
-                      <Github size={16} strokeWidth={1.5} />
-                      <span className="font-medium">
-                        {repository?.stargazerCount ||
-                          (repo && typeof repo === 'object' ? repo.stargazerCount : 0)}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              )
-            : null}
-
-        {/* Links (only show if no repo and no npm) */}
-        {!repository &&
-          !npmPackage &&
-          !(repo && typeof repo === 'object') &&
-          !(npm && typeof npm === 'object') &&
-          links && (
-            <div className="space-y-1.5">
-              <div className="text-xs text-gray-600 dark:text-gray-400">Links</div>
-              <div className="flex flex-col items-start gap-0.5 sm:flex-row sm:items-center sm:gap-1.5">
-                {links?.map(({ title, url }, idx) => (
-                  <Fragment key={url}>
-                    <Link href={url} className="flex items-center gap-1.5">
-                      <GrowingUnderline className="font-medium" data-umami-event="project-link">
-                        {title}
-                      </GrowingUnderline>
-                    </Link>
-                    {idx !== links.length - 1 && (
-                      <span className="hidden text-gray-400 dark:text-gray-500 md:inline">|</span>
-                    )}
-                  </Fragment>
-                ))}
-              </div>
-            </div>
-          )}
-
-        {/* Stack */}
-        {builtWith && builtWith.length > 0 && (
-          <div className="space-y-1.5">
-            <div className="text-xs text-gray-600 dark:text-gray-400">Stack</div>
-            <div className="flex h-6 flex-wrap items-center gap-1.5">
-              {builtWith?.map((tool) => {
-                return (
-                  <Brand
-                    key={tool}
-                    name={tool as keyof typeof BrandsMap}
-                    iconClassName={clsx(tool === 'Pygame' ? 'h-4' : 'h-4 w-4')}
-                  />
-                )
-              })}
-            </div>
-          </div>
+      <p className="mb-16 line-clamp-3 grow text-lg">{description}</p>
+      <div
+        className={clsx('mt-auto flex gap-6 sm:gap-9 md:grid md:gap-0', {
+          'md:grid-cols-1': propertyCount === 1,
+          'md:grid-cols-2': propertyCount === 2,
+          'md:grid-cols-3': propertyCount === 3,
+          'md:grid-cols-4': propertyCount === 4,
+        })}
+      >
+        {/* {projectProperty} */}
+        {npmPackageName && (
+          <NpmStats npmPackageName={npmPackageName} downloads={npmPackage?.downloads || 0} />
         )}
-
-        {/* Language */}
-        {lang && (
-          <div className="space-y-1.5">
-            <div className="text-xs text-gray-600 dark:text-gray-400">Language</div>
-            <div className="flex items-center gap-1.5">
-              <Brand name={lang.name as keyof typeof BrandsMap} as="icon" className="h-4 w-4" />
-              <span className="font-medium">{lang.name}</span>
-            </div>
-          </div>
-        )}
+        {repository && <GithubStats repository={repository} />}
+        {builtWith.length > 0 && <Stack builtWith={builtWith} />}
       </div>
     </GradientBorder>
   )
